44a2cc33639d8ad21220a9fa918f2ee4702ff732
Alisa202023 <60154547+Alisa202023@users.noreply.github.com>
2021-05-25 20:15:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+
+function someFunc() {
+var w = window.innerWidth;
+if (w > 992) {
+    jQuery('.menu__nav ul').css('display', 'flex'); 
+    
+}
+if (w < 992) {
+    jQuery('.menu__nav ul').css('display', 'none'); 
+    jQuery('.header__burger').toggleClass("active", false);
+     
+}
+}
+window.addEventListener('resize', function() { 
+    someFunc();
+});
+
+
+jQuery(document).ready(function() {
+    jQuery(".header__burger").click(function() {
+        jQuery(this).toggleClass("active");
+        jQuery('.btn__custom ').toggleClass('noactive');
+        jQuery('.call').toggleClass('noactive');
+        jQuery('.menu__nav ul').slideToggle(300, function(){
+        if(jQuery(this).css('display') === "none"){
+            jQuery(this).removeAttr('style');
+            
+        }
+        });
+    });
+}); 
